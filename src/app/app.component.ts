6a6e14363d4ed37b55e78a23f0c78e4833e597ab
Vince Campanale <vcampanale0294@gmail.com>
2017-06-01 09:30:13 -0400

    bonus challenges included

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,8 @@ import 'rxjs/add/operator/buffer';
     }
   `],
   template: `
-  <button #btn md-raised-button color="accent">Button</button>
+  <button #btn md-raised-button color="accent">Click</button>
+  <button #clearBtn md-raised-button color="primary">Clear</button>
   <div class="container">
     <h1>{{message}}</h1>
   </div>
@@ -28,6 +29,7 @@ import 'rxjs/add/operator/buffer';
 })
 export class AppComponent implements OnInit {
   @ViewChild('btn') btn;
+  @ViewChild('clearBtn') clearBtn;
   message: string;
 
   ngOnInit() {
@@ -42,32 +44,40 @@ export class AppComponent implements OnInit {
     const clickCount$ = buffered$.map(toLength);
     const doubleClick$ = clickCount$.filter(x => x === 2);
 
-    doubleClick$.subscribe(event => this.message = "Double click!");
+    doubleClick$.subscribe(event => this.message = 'Double click!');
 
-    /*
-    Using method chaining and composition:
+
     
-    click$
-      .buffer(click$.debounceTime(250))
-      .map(a => a.length)
-      .filter(x => x === 2)
-      .subscribe(e => this.message = "Double click!");
-    */
-
-    /* 
-    Bonus Challenge 1 Solution: 
-
-    */
-
-    /*
-    Bonus Challenge 2 Solution: 
-
-    const shiftClick$ = click$
-      .filter( event: KeyboardEvent => event.shiftKey )
-      .map( event => '~ Shift Clicked ~' );
+    // // Using method chaining and composition:
+    // click$
+    //   .buffer(click$.debounceTime(250))
+    //   .map(a => a.length)
+    //   .filter(x => x === 2)
+    //   .subscribe(e => this.message = "Double click!");
     
-    shiftClick$.subscribe( m => this.message = m );
-    */
+     
+    // // Bonus Challenge 1 Solution: 
+    // const clBtn = this.getNativeElement(this.clearBtn);
+    // const clear$ = Observable.fromEvent(clBtn, 'click');
+    // clear$.subscribe(e => this.message = '');
+    
+
+    // // Bonus Challenge 2 Solution: 
+    // const shiftClick$ = click$
+    //   .filter( event => event.shiftKey )
+    //   .map( event => '~ Shift Click ~' );
+    
+    // shiftClick$.subscribe( m => this.message = m );
+    
+    
+    // // Bonus Challenge 3 Solution: 
+    // const filterClickCount$ = clickStream => clickCount => clickStream
+    //     .buffer(clickStream.debounceTime(250))
+    //     .map(a => a.length)
+    //     .filter(x => x === clickCount)
+    //     .map(e => '');
+
+    // filterClickCount$(click$)(3).subscribe(e => this.message = "Triple clicked!!!");
   }
 
   getNativeElement(element) {
